5ad494b2bcc4381532f5c719d0eba75ccc698e09
franzsilva <franzsilva@gmail.com>
2018-09-19 20:33:03 -0500

    first commit

---
+++ Pages/Contact.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webapp.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; }
+        public string Hello { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your Nssse Page.";
+            Hello = "Interesting";
+        }
+    }
+}
